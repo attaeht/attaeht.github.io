eeebc890cd999777fd43e65417bb08016a451af9
attaeht <105021031+attaeht@users.noreply.github.com>
2023-08-30 10:27:19 +0100

    Update config.js

--- movie-web/config.js
+++ movie-web/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "attaeht.attaeht.workers.dev",
+  VITE_CORS_PROXY_URL: "https://attaeht.attaeht.workers.dev",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTQ2NTJkYzlmMWVlODM2ZWEwYzc1NTYyMzNlZjczZiIsInN1YiI6IjY0ZWYwNmU4M2E5OTM3MDBjNWMzNWM2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2spahnhO2EFe7Vqtf5JGst-6nXGwKXeqCrdPoCIvPkg"
 };
